c80555120447060f0f910faca1b18e0dd52d7a5f
alisterburt <alisterburt@gmail.com>
2022-10-20 00:18:24 +0100

    add fourier shift 2d implementation with example

--- src/tiltlib/fourier_shift_2d.py
+++ src/tiltlib/fourier_shift_2d.py
@@ -26,7 +26,16 @@ def get_phase_shifts_2d(shifts: torch.Tensor, image_shape: Tuple[int, int]):
     return torch.cos(factors) + 1j * torch.sin(factors)
 
 
-def phase_shift_2d(dft: torch.Tensor, shifts: torch.Tensor):
-    image_shape = dft.shape[-2:]
+def fourier_shift_dfts_2d(dfts: torch.Tensor, shifts: torch.Tensor):
+    image_shape = dfts.shape[-2:]
     phase_shifts = get_phase_shifts_2d(shifts=shifts, image_shape=image_shape)
-    return dft * phase_shifts
+    return dfts * phase_shifts
+
+
+def fourier_shift_images_2d(images: torch.Tensor, shifts: torch.Tensor):
+    images = torch.fft.fftn(images, dim=(-2, -1))
+    images = torch.fft.fftshift(images, dim=(-2, -1))
+    images = fourier_shift_dfts_2d(images, shifts)
+    images = torch.fft.ifftshift(images, dim=(-2, -1))
+    images = torch.fft.ifftn(images, dim=(-2, -1))
+    return torch.real(images)
