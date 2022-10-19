350739e074535053a673075ceb0999c19c0ae7cf
alisterburt <alisterburt@gmail.com>
2022-10-19 21:47:18 +0100

    add util to generate rotated central slice coordinates pre-arranged on image grids

--- src/tiltlib/utils/coordinates.py
+++ src/tiltlib/utils/coordinates.py
@@ -1,3 +1,4 @@
+import einops
 import torch
 from torch.nn import functional as F
 
@@ -17,3 +18,35 @@ def homogenise_coordinates(coords: torch.Tensor) -> torch.Tensor:
     Last dimension of array should be of length 3.
     """
     return F.pad(torch.Tensor(coords), pad=(0, 1), mode='constant', value=1)
+
+
+def generate_rotated_slice_coordinates(rotations: torch.Tensor, n: int) -> torch.Tensor:
+    """Generate a (batch, n, n, 3) array of rotated central slice coordinates (ordered zyx).
+
+    Parameters
+    ----------
+    rotations: torch.Tensor
+        (batch, 3, 3) array of rotation matrices.
+    n: int
+        sidelength of square grid on which coordinates are generated.
+
+    Returns
+    -------
+    coordinates: torch.Tensor
+        (batch, n, n, zyx) array of coordinates.
+    """
+    # generate [x, y, z] coordinates centered on image_sidelength // 2
+    x = y = torch.arange(n) - (n // 2)
+    xx = einops.repeat(x, 'w -> h w', h=n)
+    yy = einops.repeat(y, 'h -> h w', w=n)
+    zz = torch.zeros(size=(n, n))
+    xyz = einops.rearrange([xx, yy, zz], 'xyz h w -> 1 h w xyz 1')
+
+    # rotate coordinates
+    rotations = einops.rearrange(rotations, 'b i j -> b 1 1 i j')
+    xyz = einops.rearrange(rotations @ xyz, 'b h w xyz 1 -> b h w xyz')
+
+    # recenter
+    xyz += n // 2
+    zyx = torch.flip(xyz, dims=(-1,))
+    return zyx
