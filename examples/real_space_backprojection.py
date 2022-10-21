92ddd0e89eb65ea42c1d24ea2b6c45d37511137d
alisterburt <alisterburt@gmail.com>
2022-10-21 17:40:04 +0100

    fix bug in image stack sampling -> input coords were correct but grid shape for sampling was wrong

--- examples/real_space_backprojection.py
+++ examples/real_space_backprojection.py
@@ -3,11 +3,12 @@ import mrcfile
 import napari
 import numpy as np
 import torch
+from scipy.stats import special_ortho_group
 
 from libtilt.dft_extract_slices import slice_dft
 from libtilt.backprojection import backproject
 from libtilt.transformations import Rx, Ry, Rz, S
-from libtilt.utils.coordinates import generate_rotated_slice_coordinates, get_grid_coordinates
+from libtilt.utils.coordinates import generate_rotated_slice_coordinates, get_grid_coordinates, add_implied_coordinate_from_dimension
 
 VOLUME_FILE = 'ribo-16Apx.mrc'
 
@@ -18,13 +19,20 @@ volume_center = volume_shape // 2
 tilt_image_center = volume_center[:2]
 
 s0 = S(-volume_center)
-r0 = Rx(0)
-r1 = Ry(torch.linspace(-60, 60, steps=41))
-r2 = Rz(0)
+r0 = Rx(30)
+r1 = Ry(torch.linspace(-90, 90 - (180/30), steps=30))
+r2 = Rz(25)
 s1 = S([0, 0])
 s2 = S(tilt_image_center)
 
 projection_matrices = s2 @ s1 @ r2 @ r1 @ r0 @ s0
+
+# do random orientations instead for experiments
+# n_projections = 100
+# random_rotations = einops.repeat(torch.eye(4), 'i j -> b i j', b=n_projections).clone()
+# random_rotations[:, :3, :3] = torch.tensor(special_ortho_group.rvs(dim=3, size=n_projections))
+# projection_matrices = s2 @ random_rotations @ s0
+
 # take out rotation matrices and invert as rotating coordinates of slice in DFT rather than tomogram itself
 rotation_matrices = einops.rearrange(projection_matrices[:, :3, :3], 'b i j -> b j i')
 print(projection_matrices.shape, rotation_matrices.shape)
@@ -44,6 +52,13 @@ projections = torch.fft.ifftn(projections, dim=(1, 2))
 projections = torch.fft.ifftshift(projections, dim=(1, 2))
 projections = torch.real(projections)
 
+# xyzw = torch.tensor([24, 24, 12, 1]).reshape(4, 1).float()
+# xyzw_proj = projection_matrices @ xyzw
+# xy_proj = torch.squeeze(xyzw_proj)[..., :2] # xy
+# xyz = add_implied_coordinate_from_dimension(xy_proj, dim=0)
+# zyx = torch.flip(xyz, dims=(-1, ))
+
+
 reconstruction = backproject(
     image_stack=projections,
     projection_matrices=projection_matrices,
@@ -51,7 +66,7 @@ reconstruction = backproject(
 )
 
 viewer = napari.Viewer()
-viewer.add_image(np.array(volume))
-viewer.add_image(np.array(projections))
-viewer.add_image(np.array(reconstruction))
+volume_layer = viewer.add_image(np.array(volume), name='original 3D volume')
+# projection_layer = viewer.add_image(np.array(projections), name='projection images')
+reconstruction_layer = viewer.add_image(np.array(reconstruction), name='3D reconstruction (WBP)')
 napari.run()
--- src/libtilt/backprojection.py
+++ src/libtilt/backprojection.py
@@ -15,20 +15,20 @@ def backproject(
 ) -> torch.Tensor:
     grid_coordinates = get_grid_coordinates(output_dimensions)  # (d, h, w, zyx)
     grid_coordinates = torch.flip(grid_coordinates, dims=(-1, ))  # (d, h, w, xyz)
-    grid_coordinates = homogenise_coordinates(grid_coordinates)  # (d, h, w, zyx)
+    grid_coordinates = homogenise_coordinates(grid_coordinates)  # (d, h, w, xyzw)
     grid_coordinates = einops.rearrange(grid_coordinates, 'd h w xyzw -> d h w 1 xyzw 1')
-    projection_matrices = einops.rearrange(projection_matrices, 'img i j -> 1 img i j')[..., :2, :]
-    projected_coordinates = projection_matrices @ grid_coordinates
+    # projection_matrices = einops.rearrange(projection_matrices, 'img i j -> 1 img i j')[..., :2, :]
+    projected_coordinates = projection_matrices[..., :2, :] @ grid_coordinates  # only xy coords needed
     projected_coordinates = einops.rearrange(
         projected_coordinates, 'd h w img xy 1 -> d h w img xy'
     )
-    image_stack_coordinates = add_implied_coordinate_from_dimension(projected_coordinates, dim=-2)
+    image_stack_coordinates = add_implied_coordinate_from_dimension(projected_coordinates, dim=3)
     image_stack_coordinates = einops.rearrange(
         image_stack_coordinates, 'd h w img xyz -> img d h w xyz'
-    )  # coord for every position on grid is projected down for each image
+    )
     image_stack_coordinates = torch.flip(image_stack_coordinates, dims=(-1,))  # xyz -> zyx
     image_stack_coordinates = array_coordinates_to_grid_sample_coordinates(
-        image_stack_coordinates, array_shape=output_dimensions
+        image_stack_coordinates, array_shape=image_stack.shape
     )
     n_images = image_stack_coordinates.shape[0]
     image_stack = einops.repeat(image_stack, 'b h w -> img 1 b h w', img=n_images)  # (b, c, d, h, w) for sampling
