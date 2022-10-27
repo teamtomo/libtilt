a74c9a9fe1589c20d0b7fbb911e91f371a206bb9
alisterburt <alisterburt@gmail.com>
2022-10-27 15:49:28 +0100

    fix phase shift tests and add test for rfft phase shifts

--- src/libtilt/shift/tests/test_phase_shift_2d.py
+++ src/libtilt/shift/tests/test_phase_shift_2d.py
@@ -3,15 +3,29 @@ import torch
 from libtilt.shift.phase_shift import get_phase_shifts_2d, phase_shift_images_2d
 
 
-def test_get_phase_shifts_2d():
+def test_get_phase_shifts_2d_full_fft():
     shifts = torch.zeros(size=(1, 2))
-    phase_shifts = get_phase_shifts_2d(shifts, image_shape=(2, 2))
+    phase_shifts = get_phase_shifts_2d(shifts, image_shape=(2, 2), rfft=False)
     assert torch.allclose(phase_shifts, torch.ones(size=(2, 2), dtype=torch.complex64))
 
-    shifts = torch.ones(size=(1, 2))
-    phase_shifts = get_phase_shifts_2d(shifts, image_shape=(2, 2))
-    expected = torch.tensor([[[1. + 1.7485e-07j, -1. - 8.7423e-08j],
-                              [-1. - 8.7423e-08j, 1. + 0.0000e+00j]]])
+    shifts = torch.tensor([[1, 2]])
+    phase_shifts = get_phase_shifts_2d(shifts, image_shape=(2, 2), rfft=False)
+    expected = torch.tensor([[[1 + 0.0000e+00j, 1 + 1.7485e-07j],
+                              [-1 - 8.7423e-08j, -1 - 2.3850e-08j]]])
+    assert torch.allclose(phase_shifts, expected)
+
+
+def test_get_phase_shifts_2d_rfft():
+    shifts = torch.zeros(size=(1, 2))
+    phase_shifts = get_phase_shifts_2d(shifts, image_shape=(2, 2), rfft=True)
+    assert phase_shifts.shape == (1, 2, 2)
+    expected = torch.ones(size=(2, 2), dtype=torch.complex64)
+    assert torch.allclose(phase_shifts, expected)
+
+    shifts = torch.tensor([[1, 2]])
+    phase_shifts = get_phase_shifts_2d(shifts, image_shape=(2, 2), rfft=False)
+    expected = torch.tensor([[[1 + 0.0000e+00j, 1 + 1.7485e-07j],
+                              [-1 - 8.7423e-08j, -1 - 2.3850e-08j]]])
     assert torch.allclose(phase_shifts, expected)
 
 
