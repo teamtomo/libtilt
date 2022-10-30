7ee267006871b1f9ef58dd3c75a8d97935505f57
alisterburt <alisterburt@gmail.com>
2022-10-30 20:57:09 +0000

    add test for fftfreq grid generation

--- src/libtilt/utils/tests/test_fft.py
+++ src/libtilt/utils/tests/test_fft.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 from libtilt.utils.fft import construct_fftfreq_grid_2d, rfft_shape_from_signal_shape
 
@@ -17,3 +18,16 @@ def test_rfft_shape_from_signal_shape():
     assert rfft.shape == rfft_shape_from_signal_shape(signal_shape)
 
 
+def test_construct_fftfreq_grid_2d():
+    image_shape = (10, 30)
+    # no rfft
+    grid = construct_fftfreq_grid_2d(image_shape=image_shape, rfft=False)
+    assert grid.shape == (10, 30, 2)
+    assert torch.allclose(grid[:, 0, 0], torch.fft.fftfreq(10))
+    assert torch.allclose(grid[0, :, 1], torch.fft.fftfreq(30))
+
+    # with rfft
+    grid = construct_fftfreq_grid_2d(image_shape=image_shape, rfft=True)
+    assert grid.shape == (*rfft_shape_from_signal_shape(image_shape), 2)
+    assert torch.allclose(grid[:, 0, 0], torch.fft.fftfreq(10))
+    assert torch.allclose(grid[0, :, 1], torch.fft.rfftfreq(30))
