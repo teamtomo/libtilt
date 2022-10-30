89d1e969b9d7f6af9866b70ff1c324534b30226f
alisterburt <alisterburt@gmail.com>
2022-10-30 20:44:56 +0000

    add test for relativistic wavelength calculation

---
+++ src/libtilt/utils/tests/test_ctf.py
@@ -0,0 +1,13 @@
+from libtilt.utils.ctf import calculate_relativistic_electron_wavelength
+
+
+def test_calculate_relativistic_electron_wavelength():
+    """Check function matches expected value from literature.
+
+    De Graef, Marc (2003-03-27).
+    Introduction to Conventional Transmission Electron Microscopy.
+    Cambridge University Press. doi:10.1017/cbo9780511615092
+    """
+    result = calculate_relativistic_electron_wavelength(300e3)
+    expected = 1.969e-12
+    assert abs(result - expected) < 1e-15
