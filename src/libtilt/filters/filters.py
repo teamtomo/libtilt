8844e382b9b02de660e1972a0f08d068965a9bae
alisterburt <alisterburt@gmail.com>
2023-04-24 14:47:05 +0100

    add low_pa (#5)

--- src/libtilt/filters/filters.py
+++ src/libtilt/filters/filters.py
@@ -4,7 +4,7 @@ import torch
 from libtilt.grids import fftfreq_grid
 
 
-def bandpass_filter(
+def band_pass_filter(
     low: float,
     high: float,
     falloff: float,
@@ -35,3 +35,43 @@ def bandpass_filter(
     softened_values = torch.cos((distance / falloff) * (torch.pi / 2))
     band_float[falloff_mask] = softened_values
     return band_float
+
+
+def low_pass_filter(
+    cutoff: float,
+    falloff: float,
+    image_shape: tuple[int, int] | tuple[int, int, int],
+    rfft: bool,
+    fftshift: bool,
+    device: torch.device = None,
+) -> torch.Tensor:
+    filter = band_pass_filter(
+        low=0,
+        high=cutoff,
+        falloff=falloff,
+        image_shape=image_shape,
+        rfft=rfft,
+        fftshift=fftshift,
+        device=device
+    )
+    return filter
+
+
+def high_pass_filter(
+    cutoff: float,
+    falloff: float,
+    image_shape: tuple[int, int] | tuple[int, int, int],
+    rfft: bool,
+    fftshift: bool,
+    device: torch.device = None,
+) -> torch.Tensor:
+    filter = band_pass_filter(
+        low=cutoff,
+        high=1,
+        falloff=falloff,
+        image_shape=image_shape,
+        rfft=rfft,
+        fftshift=fftshift,
+        device=device,
+    )
+    return filter
