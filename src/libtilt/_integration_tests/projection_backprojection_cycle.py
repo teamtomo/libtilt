factor out central slice from rotation of slice and fix tests

src/libtilt/_integration_tests/projection_backprojection_cycle.py
@@ -1,13 +1,14 @@
+import einops
 import torch
 import torch.nn.functional as F
 from scipy.stats import special_ortho_group
 
-from libtilt.grids.central_slice import rotated_central_slice
+from libtilt.grids.central_slice import central_slice_grid
 
 from libtilt.projection.fourier import extract_slices
 from libtilt.shift.fourier_shift import fourier_shift_2d, phase_shift_dft_2d
 from libtilt.backprojection.fourier import insert_slices, _grid_sinc2
-from libtilt.utils.fft import symmetrised_dft_to_dft_3d
+from libtilt.utils.fft import symmetrised_dft_to_dft_3d, dft_center
 from libtilt.fsc import fsc
 
 
@@ -29,17 +30,32 @@ def test_projection_backprojection_cycle():
         p = volume.shape[0] // 4
         volume = F.pad(volume, pad=[p] * 6)
 
-    # forward model, gridding correction then make n projections
+    # rotation matrices for projection (operate on xyz column vectors)
     rotations = torch.tensor(
         special_ortho_group.rvs(dim=3, size=N_IMAGES, random_state=42)).float()
 
-    slice_coordinates = rotated_central_slice(rotations, sidelength=volume.shape[0])
+    # generate grid of coordinates for central XY slice
+    grid = central_slice_grid(sidelength=volume.shape[-1], zyx=False)  # (h, w, 3)
+
+    # rotate coordinate grid and recenter
+    rotation_matrices = einops.rearrange(rotations, 'b i j -> b 1 1 i j')
+    grid = einops.rearrange(grid, 'h w coords -> h w coords 1')
+    grid = rotation_matrices @ grid
+    grid = einops.rearrange(grid, 'b h w coords 1 -> b h w coords')
+    grid = torch.flip(grid, dims=(-1,))
+    grid = grid + dft_center(volume.shape, rfft=False, fftshifted=True)
+
+    # pre-correct for effects of subsequent Fourier space interpolation
     sinc2 = _grid_sinc2(volume.shape)
     volume *= sinc2
+
+    # calculate DFT
     dft = torch.fft.fftshift(volume, dim=(-3, -2, -1))
     dft = torch.fft.fftn(dft, dim=(-3, -2, -1))
     dft = torch.fft.fftshift(dft, dim=(-3, -2, -1))
-    dft_slices = extract_slices(dft, slice_coordinates)  # (b, h, w)
+
+    # take slices
+    dft_slices = extract_slices(dft, grid)  # (b, h, w)
 
     # shift the projections (dft slices) by phase shifting
     std = 3
@@ -76,7 +92,7 @@ def test_projection_backprojection_cycle():
         end = min(end, N_IMAGES - 1)
         reconstruction, weights = insert_slices(
             slice_data=dft_slices[start:end],
-            slice_coordinates=slice_coordinates[start:end],
+            slice_coordinates=grid[start:end],
             dft=reconstruction,
             weights=weights,
         )

src/libtilt/grids/central_slice.py
@@ -2,41 +2,30 @@ import einops
 import torch
 
 
-def rotated_central_slice(
-    rotations: torch.Tensor, sidelength: int
+def central_slice_grid(
+    sidelength: int, zyx: bool = False, device: torch.device | None = None
 ) -> torch.Tensor:
-    """Generate an array of rotated central slice coordinates for sampling a 3D image.
+    """Generate a central slice for sampling a 3D DFT.
 
-    Rotation matrices left multiply `zyx` coordinates in column vectors.
-    Coordinates returned are ordered `zyx` to match volumetric array indices.
+    The origin of the slice will be at the DFT center.
 
     Parameters
     ----------
-    rotations: torch.Tensor
-        `(batch, 3, 3)` array of rotation matrices which rotate zyx coordinates.
     sidelength: int
         Sidelength of cubic volume for which coordinates are generated.
+    zyx: bool
+        Whether coordinates should be ordered zyx (`True`) or xyz (`False`).
 
     Returns
     -------
     coordinates: torch.Tensor
-        `(batch, n, n, zyx)` array of coordinates where `n == sidelength`.
+        `(n, n, 3)` array of coordinates where `n == sidelength`.
     """
-    if rotations.ndim == 2:
-        rotations = einops.rearrange(rotations, 'i j -> 1 i j')
-    # generate [x, y, z] coordinates for a central slice
-    # the slice spans the XY plane with origin on DFT center_grid
-    x = y = torch.arange(sidelength) - (sidelength // 2)
+    x = y = torch.arange(sidelength, device=device) - (sidelength // 2)
     xx = einops.repeat(x, 'w -> h w', h=sidelength)
     yy = einops.repeat(y, 'h -> h w', w=sidelength)
-    zz = torch.zeros(size=(sidelength, sidelength))
-    grid = einops.rearrange([xx, yy, zz], 'xyz h w -> 1 h w xyz 1')
-
-    # rotate coordinates
-    rotations = einops.rearrange(rotations, 'b i j -> b 1 1 i j')
-    grid = einops.rearrange(rotations @ grid, 'b h w coords 1 -> b h w coords')
-
-    # recenter slice on DFT center_grid and flip to zyx
-    grid += sidelength // 2
-    grid = torch.flip(grid, dims=(-1,))
+    zz = torch.zeros(size=(sidelength, sidelength), device=device)
+    grid = einops.rearrange([xx, yy, zz], 'xyz h w -> h w xyz')
+    if zyx is True:
+        grid = torch.flip(grid, dims=(-1,))
     return grid

src/libtilt/grids/tests/test_central_slice.py
@@ -1,26 +1,26 @@
 import torch
-import einops
 
-from libtilt.grids.central_slice import rotated_central_slice
+from libtilt.grids.central_slice import central_slice_grid
 
 
-def test_generate_rotated_slice_coordinates():
-    # generate an unrotated slice for a 4x4x4 volume.
-    rotation = torch.eye(3)
-    slice_coordinates = rotated_central_slice(rotation, sidelength=4)
+def test_central_slice_grid():
+    # generate xyz coordinates for a (4, 4, 4) volume
+    grid_xyz = central_slice_grid(sidelength=4, zyx=False)
 
-    assert slice_coordinates.shape == (1, 4, 4, 3)
-    slice_coordinates = einops.rearrange(slice_coordinates, '1 i j zyx -> i j zyx')
-
-    # all z coordinates should be in the middle of the volume, slice is unrotated
-    assert torch.all(slice_coordinates[..., 2] == 2)
-
-    # y coordinates should be 0-3 repeated across columns
-    assert torch.all(slice_coordinates[..., 1] == torch.tensor([[0],
-                                                                [1],
-                                                                [2],
-                                                                [3]]))
+    assert grid_xyz.shape == (4, 4, 3)
 
     # x coordinates should be 0-3 repeated across rows
-    assert torch.all(slice_coordinates[..., 0] == torch.tensor([[0, 1, 2, 3]]))
+    assert torch.all(grid_xyz[..., 0] == torch.tensor([[-2, -1, 0, 1]]))
 
+    # y coordinates should be -2 to 1 repeated across columns
+    assert torch.all(grid_xyz[..., 1] == torch.tensor([[-2],
+                                                       [-1],
+                                                       [0],
+                                                       [1]]))
+
+    # all z coordinates should be 0
+    assert torch.all(grid_xyz[..., 2] == 0)
+
+    # zyx should be xyz flipped
+    grid_zyx = central_slice_grid(sidelength=4, zyx=True)
+    assert torch.allclose(torch.flip(grid_xyz, dims=(-1,)), grid_zyx)

src/libtilt/projection/tests/test_fourier.py
@@ -11,7 +11,7 @@ def test_extract_slices():
     volume = torch.zeros(4, 4, 4, dtype=torch.complex64)
 
     # insert unrotated slice
-    slice = torch.arange(4**2).reshape(4, 4)
+    slice = torch.arange(4 ** 2).reshape(4, 4)
     volume[2, :, :] = slice
 
     # construct slice coordinates
@@ -19,7 +19,8 @@ def test_extract_slices():
     xx = einops.repeat(x, 'w -> h w', h=4)
     yy = einops.repeat(y, 'h -> h w', w=4)
     zz = torch.ones_like(xx) * 2
-    slice_coordinates = einops.rearrange([zz, yy, xx], 'zyx h w -> 1 h w zyx')  # add batch dim
+    slice_coordinates = einops.rearrange([zz, yy, xx],
+                                         'zyx h w -> 1 h w zyx')  # add batch dim
 
     extracted_slice = extract_slices(dft=volume, slice_coordinates=slice_coordinates)
     error = F.mse_loss(slice, torch.real(extracted_slice.squeeze()))
@@ -37,14 +38,13 @@ def test_project_no_rotation():
     assert torch.allclose(projection, expected)
 
 
-@pytest.mark.xfail
 def test_project_with_rotation():
     volume = torch.zeros((10, 10, 10))
     volume[5, 5, 5] = 1
 
     # with rotation
-    # (5, 5, 5) is rotation center_grid so projection shouldn't change...
+    # (5, 5, 5) is rotation center so projection shouldn't change...
     rotation_matrix = torch.tensor(R.random(num=1).as_matrix()).reshape(1, 3, 3).float()
     projection = project(volume, rotation_matrix)
     expected = torch.sum(volume, dim=0)
-    assert torch.allclose(projection, expected, atol=1e-5)  # this should be closer... fft size?
+    assert torch.allclose(projection, expected, atol=1e-7)