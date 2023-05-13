factor out central slice from rotation of slice and fix tests (#25)

src/libtilt/ctf/tests/test_ctf.py
@@ -1,6 +1,8 @@
 import torch
 
 from libtilt.ctf.ctf_1d import calculate_ctf as calculate_ctf_1d
+from libtilt.ctf.relativistic_wavelength import \
+    calculate_relativistic_electron_wavelength
 
 
 def test_1d_ctf_single():
@@ -17,16 +19,16 @@ def test_1d_ctf_single():
     )
     expected = torch.tensor(
         [
-            0.1000,
-            0.1444,
-            0.2755,
-            0.4819,
-            0.7283,
-            0.9385,
-            0.9903,
-            0.7519,
-            0.1801,
-            -0.5461
+            0.1033,
+            0.1476,
+            0.2784,
+            0.4835,
+            0.7271,
+            0.9327,
+            0.9794,
+            0.7389,
+            0.1736,
+            -0.5358,
         ]
     )
     assert torch.allclose(result[0], expected, atol=1e-4)
@@ -52,3 +54,15 @@ def test_1d_ctf_batch_defocus():
           -0.9877, -0.1474]]
     )
     assert torch.allclose(result, expected, atol=1e-4)
+
+
+def test_calculate_relativistic_electron_wavelength():
+    """Check function matches expected value from literature.
+
+    De Graef, Marc (2003-03-27).
+    Introduction to Conventional Transmission Electron Microscopy.
+    Cambridge University Press. doi:10.1017/cbo9780511615092
+    """
+    result = calculate_relativistic_electron_wavelength(300e3)
+    expected = 1.969e-12
+    assert abs(result - expected) < 1e-15

src/libtilt/projection/fourier.py
@@ -3,7 +3,8 @@ import torch.nn.functional as F
 import einops
 
 from libtilt.utils.coordinates import array_to_grid_sample
-from libtilt.grids.central_slice import rotated_central_slice
+from libtilt.grids.central_slice import central_slice_grid
+from libtilt.utils.fft import dft_center
 
 
 def extract_slices(
@@ -65,21 +66,62 @@ def extract_slices(
     return samples  # (b, h, w)
 
 
-def project(volume: torch.Tensor, rotation_matrices: torch.Tensor,
-            pad=True) -> torch.Tensor:
-    """Fourier space projection by sampling central slices."""
+def project(
+    volume: torch.Tensor,
+    rotation_matrices: torch.Tensor,
+    zyx: bool = False,
+    pad: bool = True,
+) -> torch.Tensor:
+    """Project a cubic volume by sampling a central slice through its DFT.
+
+    Parameters
+    ----------
+    volume: torch.Tensor
+        `(d, d, d)` volume.
+    rotation_matrices: torch.Tensor
+        `(b, 3, 3)` which rotate coordinates of central slice to be sampled.
+    zyx: bool
+        Whether rotation matrices apply to zyx (`True`) or xyz (`False`)
+        coordinates.
+    pad: bool
+        Whether to pad the volume with zeros to increase sampling in the DFT.
+
+    Returns
+    -------
+    projections: torch.Tensor
+        `(b, d, d)` array of projection images.
+    """
+    # padding
     if pad is True:
         pad_length = volume.shape[-1] // 2
         volume = F.pad(volume, pad=[pad_length] * 6, mode='constant', value=0)
+
+    # calculate DFT
     dft = torch.fft.fftshift(volume, dim=(-3, -2, -1))
     dft = torch.fft.fftn(dft, dim=(-3, -2, -1))
     dft = torch.fft.fftshift(dft, dim=(-3, -2, -1))
-    slice_coordinates = rotated_central_slice(rotation_matrices,
-                                              sidelength=dft.shape[-1])
-    projections = extract_slices(dft, slice_coordinates)  # (b, h, w)
+
+    # generate grid of coordinates for central XY slice
+    grid = central_slice_grid(
+        sidelength=dft.shape[-1], zyx=zyx, device=volume.device
+    )  # (h, w, 3)
+
+    # rotate coordinate grid and recenter
+    rotation_matrices = einops.rearrange(rotation_matrices, 'b i j -> b 1 1 i j')
+    grid = einops.rearrange(grid, 'h w coords -> h w coords 1')
+    grid = rotation_matrices @ grid
+    grid = einops.rearrange(grid, 'b h w coords 1 -> b h w coords')
+    if zyx is False:
+        grid = torch.flip(grid, dims=(-1, ))
+    grid = grid + dft_center(volume.shape, rfft=False, fftshifted=True)
+
+    # sample slices from DFT
+    projections = extract_slices(dft, grid)  # (b, h, w)
     projections = torch.fft.ifftshift(projections, dim=(-2, -1))
     projections = torch.fft.ifftn(projections, dim=(-2, -1))
     projections = torch.fft.ifftshift(projections, dim=(-2, -1))
+
+    # unpadding
     if pad is True:
         projections = projections[:, pad_length:-pad_length, pad_length:-pad_length]
     return torch.real(projections)

src/libtilt/utils/tests/test_fft.py
@@ -139,7 +139,7 @@ def test_symmetrised_dft_to_dft_3d(inplace: bool):
     )
     desymmetrised_dft = symmetrised_dft_to_dft_3d(symmetrised_dft,
                                                   inplace=inplace)
-    assert torch.allclose(desymmetrised_dft, fft, atol=1e-6)
+    assert torch.allclose(desymmetrised_dft, fft, atol=1e-5)
 
 
 @pytest.mark.parametrize(
@@ -154,7 +154,7 @@ def test_symmetrised_dft_to_dft_3d_batched(inplace: bool):
         image, dim=(-3, -2, -1)), dim=(-3, -2, -1))
     desymmetrised_dft = symmetrised_dft_to_dft_3d(symmetrised_dft,
                                                   inplace=inplace)
-    assert torch.allclose(desymmetrised_dft, fft, atol=1e-6)
+    assert torch.allclose(desymmetrised_dft, fft, atol=1e-5)
 
 
 @pytest.mark.parametrize(
@@ -172,7 +172,7 @@ def test_symmetrised_dft_to_dft_3d_batched(inplace: bool):
 )
 def test_fft_center(fftshifted, rfft, input, expected):
     result = dft_center(input, fftshifted=fftshifted, rfft=rfft)
-    assert torch.allclose(result, expected)
+    assert torch.allclose(result, expected.long())
 
 
 def test_fftfreq_to_spatial_frequency():