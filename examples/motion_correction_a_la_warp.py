f8dece78ff426c26efd9aee257eefd9439839e70
alisterburt <alisterburt@gmail.com>
2023-05-15 21:49:42 +0100

    add comments to motion correction example

--- examples/motion_correction_a_la_warp.py
+++ examples/motion_correction_a_la_warp.py
@@ -21,40 +21,42 @@ N_ITERATIONS = 200
 N_PATCHES_PER_BATCH = 15
 LEARNING_RATE = 0.05
 
+# load data, bin 8x, reduce to 2D and normalise
 image = torch.tensor(mrcfile.read(IMAGE_FILE)).float()
 image = F.avg_pool2d(image, kernel_size=8)
 image = einops.reduce(image, 't h w -> h w', reduction='mean')
 image = (image - torch.mean(image)) / torch.std(image)
 
-### simulate continuous beam induced motion on the target
+# simulate continuous beam induced motion on the image
+# make image multi-frame, initialise a deformation field with random shifts
 multi_frame_micrograph = einops.repeat(image, 'h w -> 10 h w').clone()
-t, h, w = multi_frame_micrograph.shape[-3:]
-
 gt_deformation_field_data = np.random.uniform(
     low=-5, high=5, size=(2, *GT_DEFORMATION_FIELD_RESOLUTION)
 )
 gt_deformation_field = CubicBSplineGrid3d.from_grid_data(
     torch.tensor(gt_deformation_field_data).float()
 )
+
+# evaluate deformation field at every pixel
+t, h, w = multi_frame_micrograph.shape[-3:]
 _t, _y, _x = torch.linspace(0, 1, t), torch.linspace(0, 1, h), torch.linspace(0, 1, w)
 tt, yy, xx = torch.meshgrid(_t, _y, _x, indexing='ij')
 tyx = einops.rearrange([tt, yy, xx], 'tyx t h w -> t h w tyx')
-
-predicted_shifts = gt_deformation_field(tyx)
+shifts = gt_deformation_field(tyx)
 array_coordinates = torch.tensor(np.indices(image.shape[-2:]))
 array_coordinates = einops.rearrange(array_coordinates, 'yx ... -> ... yx')
 
-deformed_coordinates = array_coordinates + predicted_shifts
-grid_sample_coordinates = array_to_grid_sample(deformed_coordinates, array_shape=(h, w))
+# find coordinates to sample (after deforming by shifts)
+deformed_coordinates = array_coordinates + shifts
 
+# sample image at deformed positions
 multi_frame_micrograph = F.grid_sample(
     input=einops.rearrange(multi_frame_micrograph, 't h w -> t 1 h w'),
-    grid=grid_sample_coordinates,
+    grid=array_to_grid_sample(deformed_coordinates, array_shape=(h, w)),
     mode='bicubic',
     padding_mode='zeros',
     align_corners=True,
 )
-
 multi_frame_micrograph = einops.rearrange(multi_frame_micrograph, 't 1 h w -> t h w')
 
 # prepare data for learning the motion...
@@ -69,15 +71,13 @@ data_patches, data_patch_centers = patch_grid(
     distribute_patches=True,
 )
 data_patches = einops.rearrange(data_patches, 't gh gw 1 ph pw -> t gh gw ph pw')
-gh, gw = data_patch_centers.shape[1:3]
-
 reference_patches, _ = patch_grid(
     images=reference,
     patch_shape=(PATCH_SIDELENGTH, PATCH_SIDELENGTH),
     patch_step=(PATCH_SIDELENGTH // 2, PATCH_SIDELENGTH // 2),
     distribute_patches=True,
 )  # (grid_h, grid_w, ph, pw)
-
+gh, gw = data_patch_centers.shape[1:3]
 
 # apply a soft circular mask on both reference and data patches
 mask = circle(
@@ -92,7 +92,6 @@ reference_patches *= mask
 data_patches = torch.fft.rfftn(data_patches, dim=(-2, -1))
 reference_patches = torch.fft.rfftn(reference_patches, dim=(-2, -1))
 
-
 # initialise the deformation field with learnable parameters and normalise
 # patch centers to [0, 1] for evaluation of shifts.
 deformation_field = CubicBSplineGrid3d(
@@ -100,7 +99,6 @@ deformation_field = CubicBSplineGrid3d(
     n_channels=2
 )
 data_patch_centers = data_patch_centers / torch.tensor([t - 1, h - 1, w - 1])
-
 
 # initialise optimiser and detach data
 motion_optimiser = torch.optim.Adam(
