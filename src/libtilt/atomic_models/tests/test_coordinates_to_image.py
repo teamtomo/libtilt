5e1f89e5c6f539bf866fe2dcb608a545689f832e
alisterburt <alisterburt@gmail.com>
2023-05-21 18:18:05 +0100

    add API for rendering coordinate data as images

---
+++ src/libtilt/atomic_models/tests/test_coordinates_to_image.py
@@ -0,0 +1,22 @@
+import torch
+
+from libtilt.atomic_models.coordinates_to_image import (
+    coordinates_to_image_2d, coordinates_to_image_3d
+)
+
+
+def test_coordinates_to_image_2d():
+    coordinates = torch.as_tensor([14, 14])
+    image = coordinates_to_image_2d(coordinates=coordinates, image_shape=(28, 28))
+    expected = torch.zeros((28, 28))
+    expected[14, 14] = 1
+    assert image.shape == (28, 28)
+    assert torch.allclose(image, expected)
+
+
+def test_coordinates_to_image_3d():
+    coordinates = torch.as_tensor([14, 14, 14])
+    image = coordinates_to_image_3d(coordinates=coordinates, image_shape=(28, 28, 28))
+    expected = torch.zeros((28, 28, 28))
+    expected[14, 14, 14] = 1
+    assert torch.allclose(image, expected)
