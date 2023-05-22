3c4381403578dcc17ca3dfdfbcea3c6020ddfd6c
Josh Dickerson <jdickerson@mrc-lmb.cam.ac.uk>
2023-05-22 17:24:59 +0100

    Add test for bfactor_2d (#38)
    * Playing with code. Added b factor envelope. No test yet.
    
    * Added a test for b factor. Seems OK
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Josh Dickerson <josh@Joshs-MacBook-Pro.local>
    Co-authored-by: alisterburt <alisterburt@gmail.com>

---
+++ src/libtilt/filters/tests/test_b.py
@@ -0,0 +1,22 @@
+import torch
+
+from libtilt.filters import bfactor_2d
+
+def test_bfactor_2d():
+    # Generate an image
+    image = torch.zeros((4,4))
+    image[1:, :] = 1
+    # apply bfactor
+    result = bfactor_2d(
+        image=image,
+        B=10,
+        pixel_size=1,
+    )
+    expected = torch.tensor(
+        [[0.18851198,0.18851198,0.18851198,0.18851198,],
+         [0.88381535,0.88381535,0.88381535,0.88381535],
+         [1.0438573,1.0438573,1.0438573,1.0438573],
+         [0.88381535,0.88381535,0.88381535,0.88381535]
+         ]
+    )
+    assert torch.allclose(result, expected, atol=1e-3)
