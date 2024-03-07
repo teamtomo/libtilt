787698a9108612e72d62931a753a14553d758e8f
McHaillet <martenchaillet@gmail.com>
2024-03-07 18:02:27 +0100

    add some checks for upsampling

--- src/libtilt/alignment/find_shift.py
+++ src/libtilt/alignment/find_shift.py
@@ -12,7 +12,11 @@ def find_image_shift(
         image_b: torch.Tensor,
         upsampling_factor: float = 10,
 ) -> torch.Tensor:
-    """
+    """Find the shift between two images. The shift specifies how far image_b is
+    shifted relative to image_a. Applying this shift to image_a will align it with
+    image_b. Applying the inverse shift to b, will align it with image_a. The
+    region around the maximum in the correlation image is by default upsampled with
+    bicubic interpolation to find a more precise shift.
 
     Parameters
     ----------
@@ -21,14 +25,18 @@ def find_image_shift(
     image_b: torch.Tensor
         `(y, x)` image with the same shape as image_a
     upsampling_factor: float
-        how many times the correlation image is upsampled with bicubic
-        interpolation to find an interpolated shift
+        How many times the correlation image is upsampled with bicubic
+        interpolation to find an interpolated shift. The value needs to be larger or
+        equal to 1.
 
     Returns
     -------
-    projections: torch.Tensor
-        `(..., d, d)` array of projection images.
+    shift: torch.Tensor
+        `(2, )` shift in y and x.
     """
+    if upsampling_factor < 1:
+        raise ValueError('Upsampling factor for finding a shift between two images '
+                         'cannot be smaller than 1.')
     image_shape = image_a.shape
     center = dft_center(
         image_shape, rfft=False, fftshifted=True, device=image_a.device
@@ -43,10 +51,13 @@ def find_image_shift(
         device=image_a.device
     )
     shift = center - maximum_idx
-    if torch.any(maximum_idx < 2) or torch.any(image_shape - maximum_idx < 3):
+    if upsampling_factor == 1:
+        return shift
+    elif torch.any(maximum_idx < 2) or torch.any(image_shape - maximum_idx < 3):
         # if the maximum is too close to the border, it cannot be upsampled
         return shift
-    else:  # find interpolated shift by upsampling the correlation image
+    else:
+        # find interpolated shift by upsampling the correlation image
         peak_region_y = slice(maximum_idx[0] - 2, maximum_idx[0] + 3)
         peak_region_x = slice(maximum_idx[1] - 2, maximum_idx[1] + 3)
         upsampled = F.interpolate(
