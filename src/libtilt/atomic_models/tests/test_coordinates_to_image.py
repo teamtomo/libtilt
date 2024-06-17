8f11d8645b87df6acb22154c6008e4d13b2dcfc9
McHaillet <martenchaillet@gmail.com>
2024-06-17 17:30:00 +0200

    add atomic models tests

--- src/libtilt/atomic_models/tests/test_coordinates_to_image.py
+++ src/libtilt/atomic_models/tests/test_coordinates_to_image.py
@@ -3,8 +3,10 @@ import torch
 from libtilt.atomic_models.coordinates_to_image import (
     coordinates_to_image_2d, coordinates_to_image_3d
 )
+from libtilt.pytest_utils import device_test
 
 
+@device_test
 def test_coordinates_to_image_2d():
     coordinates = torch.as_tensor([14, 14])
     image = coordinates_to_image_2d(coordinates=coordinates, image_shape=(28, 28))
@@ -14,6 +16,7 @@ def test_coordinates_to_image_2d():
     assert torch.allclose(image, expected)
 
 
+@device_test
 def test_coordinates_to_image_3d():
     coordinates = torch.as_tensor([14, 14, 14])
     image = coordinates_to_image_3d(coordinates=coordinates, image_shape=(28, 28, 28))
