8ab4b55cd2c3e081040fa707a88caf61b07e277a
McHaillet <martenchaillet@gmail.com>
2024-06-17 17:52:08 +0200

    add fsc and fix for split idx

--- src/libtilt/fsc/_tests/test_fsc.py
+++ src/libtilt/fsc/_tests/test_fsc.py
@@ -1,20 +1,24 @@
 import torch
 
 from libtilt.fsc.fsc import fsc
+from libtilt.pytest_utils import device_test
 
 
+@device_test
 def test_fsc_identical_images():
     a = torch.rand(size=(10, 10))
     result = fsc(a, a)
     assert torch.allclose(result, torch.ones(6))
 
 
+@device_test
 def test_fsc_identical_volumes():
     a = torch.rand(size=(10, 10, 10))
     result = fsc(a, a)
     assert torch.allclose(result, torch.ones(6))
 
 
+@device_test
 def test_fsc_identical_images_with_index_subset():
     a = torch.rand(size=(10, 10))
     rfft_mask = torch.zeros(size=(10, 6), dtype=torch.bool)
@@ -24,6 +28,7 @@ def test_fsc_identical_images_with_index_subset():
     assert torch.allclose(result, torch.ones(6))
 
 
+@device_test
 def test_fsc_identical_volumes_with_index_subset():
     a = torch.rand(size=(10, 10, 10))
     rfft_mask = torch.zeros(size=(10, 10, 6), dtype=torch.bool)
